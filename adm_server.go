158e9b5c25fa49791b0be4fb3dae1019769f0368
mishukoffs <Mis.Serj@yandex.ru>
2024-10-09 12:48:05 +0300

    Инициализация subscribers и мьютекса вынесена в главную функцию

--- adm_server.go
+++ adm_server.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type AdminServ struct {
 	logger SimpleEventLogger
@@ -47,7 +50,12 @@ func (adm *AdminServ) Statistics(interval *StatInterval, stream Admin_Statistics
 }
 
 func getAdminInstance() *AdminServ {
-	return &AdminServ{}
+	return &AdminServ{
+		logger: SimpleEventLogger{
+			mu:          sync.Mutex{},
+			subscribers: make(map[chan *Event]struct{}),
+		},
+	}
 }
 
 func emptyStat() *Stat {
